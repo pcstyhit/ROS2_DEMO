6678bd901733bb8b7cf097eeb12c092c76b136c6
pldz <pldz@R7000.com>
2023-05-02 18:22:34 +0800

    2023/05/02 Update the ros2 c/c++ project quickly strat and ros2 topic communication base on default information

---
+++ 2_Chapter/code/build/cppBaseMsgSub/CMakeFiles/cppBaseMsgSubNode.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cppBaseMsgSubNode.dir/src/cppBaseMsgSubNode.cpp.o"
+  "CMakeFiles/cppBaseMsgSubNode.dir/src/cppBaseMsgSubNode.cpp.o.d"
+  "cppBaseMsgSubNode"
+  "cppBaseMsgSubNode.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cppBaseMsgSubNode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 2_Chapter/code/build/cppBaseMsgSub/CMakeFiles/cppBaseMsgSub_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cppBaseMsgSub_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/cppBaseMsgSub_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
